a46639ab2edf5b58f4c6cb4e4daf9f517c22ac93
mozo-1701 <mozo1701@yh.nackademin.se>
2019-01-11 00:25:05 +0100

    19-01-02c

--- 2019-01-02c.sh
+++ 2019-01-02c.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo " The IPv6 address is: "
-ip addr | awk '/^[0-9]+/ { currentinterface=$2 } $1 == "inet" { split( $2, foo, "/" ); print currentinterface ,foo[1] }'
+ip addr | awk '/^[0-9]+/ { currentinterface=$2 } $1 == "inet6" { split( $2, foo, "/" ); print currentinterface ,foo[1] }'
