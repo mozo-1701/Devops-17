c90c7aad8a799743f4cd55a4d5c105204b00210a
mozo-1701 <mozo1701@yh.nackademin.se>
2019-01-12 00:12:20 +0100

    190107-labb1f

--- labb/190107-labb1f.sh
+++ labb/190107-labb1f.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-while true; do
-read input
-echo "Printing down $input to 1"
-j=$input
-while [ $j -ge 1 ]
+while true; 
 do
+  printf "Give a number:  "
+  read input
+  echo "Printing down $input to 1"
+j=$input
+   while [ $j -ge 1 ]
+   do
    echo -n "$j "
    j=$(( j - 1 )) # decrease number by 1 
+   done
+printf "\n"
 done
-done
-echo ""
+
 
