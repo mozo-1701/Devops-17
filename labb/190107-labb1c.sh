9973c514d779ef0005de4c87edd0ca7898830b0b
mozo-1701 <mozo1701@yh.nackademin.se>
2019-01-12 12:45:09 +0100

    190107-labb1c

--- labb/190107-labb1c.sh
+++ labb/190107-labb1c.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-echo "printing out 10 to 1"
-j=10
-while [ $j -ge 1 ]
-do
-   echo -n "$j "
-   j=$(( j - 1 )) # decrease number by 1 
-done
 
-echo ""
+#!/bin/bash
+echo "Usage ./script < string > " 
+o=
+for i;do
+    o="$i $o"
+    done
+
+printf "%s " $o
+printf "\n"
+
