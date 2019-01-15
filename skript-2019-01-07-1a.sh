2c2a5b7319464fad71104613dc1174ca406e0e34
mozo-1701 <mozo1701@yh.nackademin.se>
2019-01-15 23:50:38 +0100

    019-01-07-1a

---
+++ skript-2019-01-07-1a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Usage: <./script> "
+echo "Ange username: "
+# Reading from standard input
+read username
+
+# Cheking if username is already logged in or not?
+if [ $username = $USER ]; then
+   echo " User: $username is  logged in";
+
+else
+
+   echo " User: $username is not logged in";
+fi
