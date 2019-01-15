4aeb31ec2332a3f86a3921b843ee86f510217f11
mozo-1701 <mozo1701@yh.nackademin.se>
2019-01-15 23:57:23 +0100

    2019-01-07-1g

---
+++ skript-2019-01-07-1g.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Usage ./script <text> <integer>"
+
+  echo "Printing down $2 to 1"
+# Run the loop as long as j is greater or equal 1 .
+j=$2
+   while [ $j -ge 1 ]
+   do
+     echo -n "$j "
+     j=$(( j - 1 )) # decrease number by 1 
+   done
+printf "\n"
+
+
