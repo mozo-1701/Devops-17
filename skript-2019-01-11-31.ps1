eaf3c10f34a9b69ba2002214eccffd6e232aee6c
mozo-1701 <mohammad.zohouri@yh.nackademin.se>
2019-01-23 19:37:26 +0100

    2019-01-11-31

---
+++ skript-2019-01-11-31.ps1
@@ -0,0 +1,22 @@
+3.1	
+3.1.1
+	
+	
+PS C:\Users\Administrator.VT17_2>  write-host "Hello World"
+Hello World
+	
+
+3.1.2
+
+PS C:\write-host -foregroundcolor yellow "Hello World"
+PS C:\Users\Administrator.VT17_2> write-host -foregroundcolor yellow "Hello World"
+Hello World
+
+3.1.3
+ 
+ PS C:\Users\Administrator.VT17_2> write-host -foregroundcolor blue -backgroundcolor yellow "Hello World"
+Hello World
+
+3.1.4
+
+Clear-Host on line comes up on predefined broswer (t.ex. chrome )
